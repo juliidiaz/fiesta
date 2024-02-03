656d133056f5f78c2f117cc3421af53b9b056d4d
Julii Díaz <juliana.diaz@live.com>
2024-02-03 19:06:28 -0300

    disenos

--- app.js
+++ app.js
@@ -5,8 +5,11 @@ const PORT = process.env.PORT || 3000;
 
 // Rutas
 const mainRoute =require('./src/routes/main');
+const disenosRoute =require('./src/routes/disenos');
+
 
 app.use('/', mainRoute);
+app.use('/disenos', disenosRoute);
 
 //PUBLIC
 app.use(express.static(path.join(__dirname, 'public')));
---
+++ src/controllers/disenosController.js
@@ -0,0 +1,7 @@
+const disenosController = {
+    bodaFestivo: (req, res)=>{
+        res.render('disenos/boda-festivo')
+    }
+}
+
+module.exports=disenosController;
